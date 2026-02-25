683c5e6ac63e31f2d7a6da688151293d4c0713f6
david musembi <dmusembi702@gmail.com>
2026-02-25 22:16:53 +0300

    styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import { useState, useEffect } from "react";
 import { format } from "date-fns";
 
 function App() {
-  return (
-    <p className = "time">
-      {format(new Date(), "yyyy-MM-dd HH:mm:ss"
-)}
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+
+  }, []);
+  
+    return (
+      <div className="container">
+  <p className="time">
+      {format(time, "yyyy-MM-dd HH:mm:ss")}
     </p>
-  );
+      </div>
+  ); 
+  
+ 
 }
 
 export default App;
-
-
